3b04b4bced3e2e6f22c002818197fa98b5b3a14c
Chirag Bansal <chiragb3005@gmail.com>
2026-04-03 19:00:27 +0530

    done with small changes

---
+++ 01npx_react/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <h1>chai aur core react | chirag bansal</h1>
+  );
+};
+
+
+function Chai() {
+  return (
+    <h3>mujhe acha lga hai</h3>
+  )
+};
+
+export default App;
+export {Chai};
