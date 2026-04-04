bb844a4f386aa55ef0591b2d77a9ab880ac48b2a
Chirag Bansal <chiragb3005@gmail.com>
2026-04-04 12:39:56 +0530

    small changes in vite react

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,8 +1,14 @@
+const username = 'chiragB'
+
 
 function App() {
 
   return (
-    <h1>chai aur vite</h1>
+    <>
+    <h1>chai aur vite {username}</h1>
+    {/* only final outcome can be eject here no logic do that outside return */}
+    <p>test para</p>
+    </>
   )
 }
 
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -1,10 +1,51 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Chai from './Chai.jsx'
 
+
+function MyApp(){
+    return(
+        <div>
+            <h2>Custom App !</h2>
+        </div>
+    )
+}
+    
+
+
+// const ReactElement = {
+//     type : 'a' ,
+//     props : {
+//         href : 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+// Here custom render of react will not accept this type of syntax
+
+const AnotherElement = (
+    <a href="https://google.com" target='_blank'>visit google</a>
+)
+
+const AnotherUser = 'DPS'
+
+const ReactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'Click me to visit google',
+    <br />, <br />,
+    AnotherUser
+)
+
+
+
 createRoot(document.getElementById('root')).render(
+    
     <>
     <App />
+    {ReactElement}
     <Chai />
     </>
+
 )
